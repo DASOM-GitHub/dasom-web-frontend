e01f161cb9c59c211eb8d5a7658da86ce4ad086a
DESKTOP-9E5KB98\user <b01023320838@gmail.com>
2025-02-08 23:16:02 +0900

    <MobileLayout> 제거 <div> 추가

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import MobileLayout from '../layout/MobileLayout'
 import headerMenu from '../../assets/images/headerMenu.svg'
 
 export const Header = (): JSX.Element => {
     return (
-        <MobileLayout>
-            <div className="ml-[17px] pt-[53px] w-[93px] h-8 font-black text-mainColor text-2xl">
+        <div className="flex justify-between items-center px-4 py-4">
+            <div className="font-black text-mainColor text-2xl">
                 DASOM
             </div>
 
             <img
-                className='relative w-3 h-[9px] top-[12px] left-[351px]' 
+                className="w-3 h-[9px]" 
                 alt="headerMenu"
                 src={headerMenu}
             />
-        </MobileLayout>
+        </div>
     )
 }
