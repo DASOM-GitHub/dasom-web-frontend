7f2d46143ad990036c80625a6f4d048fda996764
HYEON <titeotty12@gmail.com>
2025-02-10 01:19:44 +0900

    fix: Header 및 MobileLayout 반응형으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './assets/styles/index.css'
 import Main from './pages/Main'
-import CoreMembers from './pages/CoreMembers'
 
 function App() {
   return (
     <div className="App">
-      <CoreMembers />
+      <Main />
     </div>
   )
 }
