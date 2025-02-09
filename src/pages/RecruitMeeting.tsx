659f7989ba51ed2e77a082d5da416b2ab5a52ed1
limtjdghks <epaktldk0203@gmail.com>
2025-02-09 22:48:25 +0900

    feat: 면접예약 페이지 컴포넌트 구현현

---
+++ src/pages/RecruitMeeting.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import MobileLayout from '../components/layout/MobileLayout'
+import MeetingDateSelector from '../components/layout/MeetingDateSelector'
+import MeetingTimeSelector from '../components/layout/MeetingTimeSelector'
+import { useNavigate } from 'react-router-dom'
+import { Header } from '../components/UI/Header'
+
+/** 면접 날짜 선택 페이지 */
+const RecruitMeeting: React.FC = () => {
+	const navigate = useNavigate()
+	const [selectedDate, setSelectedDate] = useState<string>('')
+	const [selectedTime, setSelectedTime] = useState<string>('')
+
+	/** 날짜 선택 핸들러 */
+	const handleDateSelect = (date: string) => {
+		setSelectedDate(date)
+	}
+
+	/** 시간 선택 핸들러 */
+	const handleTimeSelect = (time: string) => {
+		setSelectedTime(time)
+	}
+
+	/** 면접일정 폼 제출 핸들러 */
+	const handleSubmit = () => {
+		// 선택된 날짜와 시간 state값 전달하여 페이지 이동
+		navigate('/recruit-meeting/submit', { state: { date: selectedDate, time: selectedTime } })
+	}
+
+	return (
+		<MobileLayout>
+			<Header />
+			<div className='flex flex-col items-center w-full'>
+				<p className='font-pretendardBold text-white text-center bg-mainColor max-w-[90%] w-full mt-6'>
+					1차 서류에 합격되신 점 다시 한번 축하드리며,
+					<br />
+					편하신 날짜의 시간대를 선택해주시길 바랍니다.
+				</p>
+				<div className='mt-3 max-w-[90%]'>
+					<p className='font-pretendardBold text-white mb-4'>면접일</p>
+					<MeetingDateSelector onSelect={handleDateSelect} />
+					<br />
+					<p className='font-pretendardBold text-white mb-4'>시간</p>
+					<MeetingTimeSelector onSelect={handleTimeSelect} />
+				</div>
+				<button className='font-pretendardBold text-white text-center bg-mainColor mt-10 p-1 w-[140px] h-[30px]' onClick={handleSubmit}>
+					면접일정 예약하기
+				</button>
+			</div>
+		</MobileLayout>
+	)
+}
+
+export default RecruitMeeting
