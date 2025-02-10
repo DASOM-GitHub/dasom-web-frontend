99915eae0baac9befa4a663e4839ffb6be2d1b87
HYEON <titeotty12@gmail.com>
2025-02-10 22:08:21 +0900

    infra: Cover Library 추가 및 Header, Main 레이아웃 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Header } from '../components/UI/Header'
+import { Cover } from '../components/UI/cover'
+import MobileLayout from '../components/layout/MobileLayout'
 
 const Main: React.FC = () => {
   return (
-    <div>
+    <MobileLayout>
       <Header/>
-    </div>
+      <div className="top-44 text-4xl md:text-[24px] lg:text-[36px] font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 via-neutral-400 to-neutral-300 dark:from-neutral-800 dark:via-white dark:to-white">
+        Code Beyond Limits, <br/> Discover <Cover>New Spaces</Cover> with <div className='text-mainColor'>DASOM</div>
+      </div>
+    </MobileLayout>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    'app/**/*.{ts,tsx}', 
+    'components/**/*.{ts,tsx}'
   ],
     theme: {
       extend: {
@@ -13,6 +15,46 @@ export default {
             subGrey: '#F5F5F5',
             subGrey2: '#A8A8A8',
             subGrey3: '#26262D',
+
+            border: "hsl(var(--border))",
+            input: "hsl(var(--input))",
+            ring: "hsl(var(--ring))",
+            background: "hsl(var(--background))",
+            foreground: "hsl(var(--foreground))",
+
+            primary: {
+              DEFAULT: "hsl(var(--primary))",
+              foreground: "hsl(var(--primary-foreground))",
+            },
+            secondary: {
+              DEFAULT: "hsl(var(--secondary))",
+              foreground: "hsl(var(--secondary-foreground))",
+            },
+            destructive: {
+              DEFAULT: "hsl(var(--destructive))",
+              foreground: "hsl(var(--destructive-foreground))",
+            },
+            muted: {
+              DEFAULT: "hsl(var(--muted))",
+              foreground: "hsl(var(--muted-foreground))",
+            },
+            accent: {
+              DEFAULT: "hsl(var(--accent))",
+              foreground: "hsl(var(--accent-foreground))",
+            },
+            popover: {
+              DEFAULT: "hsl(var(--popover))",
+              foreground: "hsl(var(--popover-foreground))",
+            },
+            card: {
+              DEFAULT: "hsl(var(--card))",
+              foreground: "hsl(var(--card-foreground))",
+            },
+        },
+        borderRadius: {
+          lg: `var(--radius)`,
+          md: `calc(var(--radius) - 2px)`,
+          sm: "calc(var(--radius) - 4px)",
         },
         fontFamily: {
           pretendardBlack: ['Pretendard Black', 'sans-serif'],
@@ -22,5 +64,5 @@ export default {
         }
       },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
   }
