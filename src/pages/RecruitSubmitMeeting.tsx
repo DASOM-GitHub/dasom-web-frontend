55d1c2fd7d1ebdc1f26efd9004ad0b795fe28d6d
limtjdghks <84004766+limtjdghks@users.noreply.github.com>
2025-02-17 23:22:46 +0900

    fix: 스크롤 이상현상 수정 + 페이지 레이아웃 수정
    * fix: 스크롤 이상현상 수정
    
    * fix: 페이지 레이아웃 수정

--- src/pages/RecruitSubmitMeeting.tsx
+++ src/pages/RecruitSubmitMeeting.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import MobileLayout from '../components/layout/MobileLayout'
 import { Header } from '../components/UI/Header'
+import { RecruitHeader, RecruitUI } from '../components/UI/RecruitUI'
 
 const RecruitSubmitMeeting: React.FC = () => {
 	const location = useLocation()
@@ -9,7 +10,9 @@ const RecruitSubmitMeeting: React.FC = () => {
 
 	return (
 		<MobileLayout>
-			<div className='flex flex-col items-center w-full mt-6'>
+			<RecruitHeader title='컴퓨터 소프트웨어 공학과 전공 동아리 다솜 34기 모집 폼' />
+			<RecruitUI />
+			<div className='flex flex-col items-center w-full mt-12'>
 				<p className='font-pretendardBold text-white text-center bg-mainColor w-[90%]'>
 					면접일 제출이 완료되었습니다.
 					<br />
