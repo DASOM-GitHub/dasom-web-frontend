028329d40c0a0c1f7836c722956b7d732b6ed57c
sooh329 <glay@kakao.com>
2025-02-22 18:13:36 +0900

    feat: 일정 api 연동 중

--- src/pages/RecruitResult.tsx
+++ src/pages/RecruitResult.tsx
@@ -19,12 +19,12 @@ export const RecruitResult: React.FC = () => {
       <Header />
       <RecruitHeader title="컴퓨터 소프트웨어 공학과 전공 동아리 다솜 34기 합격자 조회" />
       <form className="mt-4 bg-mainBlack w-full px-2" onSubmit={handleSubmit}>
-        {/* <InputField
+        <InputField
           label="학번 마지막 4자리 + 전화번호 마지막 4자리를 입력해주세요."
           subLabel='ex) 08470542'
           type='test'
           required
-        /> */}
+        />
         <Button text="결과 확인하기"/>
       </form>
     </MobileLayout>
