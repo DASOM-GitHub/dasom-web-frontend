1b1758f29ae4ecb127cd8a914b85ae2aa9087f26
limtjdghks <epaktldk0203@gmail.com>
2025-02-23 22:54:12 +0900

    feat: 면접 일정 API 연동

--- src/components/UI/MeetingDateSelector.tsx
+++ src/components/UI/MeetingDateSelector.tsx
@@ -1,4 +1,4 @@
-import React, { JSX, useState } from 'react'
+import React, { JSX, useEffect, useState } from 'react'
 import MeetingDate from './MeetingDate'
 
 interface dateInfo {
@@ -19,16 +19,43 @@ interface props {
 	period: interviewPeriod
 }
 
-/** 면접 날짜 목록 */
-const meetingDates: dateInfo[] = [
-	{ month: '3', day: '19', week: '수요일' },
-	{ month: '3', day: '20', week: '목요일' },
-	{ month: '3', day: '21', week: '금요일' },
-]
-
 /** 면접 날짜 선택 레이아웃 */
 const MeetingDateSelector = ({ onSelect, period }: props): JSX.Element => {
 	const [selectedDate, setSelectedDate] = useState<string>('')
+	const [meetingDates, setMeetingDates] = useState<dateInfo[]>([])
+	
+	/** 날짜 사이의 일 수 계산 */
+	const getDateDiff = (d1: string, d2: string) => {
+			const date1 = new Date(d1)
+			const date2 = new Date(d2)
+
+			const diffDate = date1.getTime() - date2.getTime()
+
+			return Math.abs(diffDate / (1000 * 60 * 60 * 24))
+	}
+
+	// 면접 예약 일자 목록 가져오기
+	useEffect(() => {
+		const startDate = new Date(period.periodStart)
+		const dateArray: dateInfo[] = []
+
+		for (let i = 0; i <= getDateDiff(period.periodStart, period.periodEnd); i++) {
+			const date = new Date(startDate)
+			date.setDate(startDate.getDate() + i)
+
+			dateArray.push({
+				month: (date.getMonth() + 1).toString(),
+				day: date.getDate().toString(),
+				week: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'][date.getDay()],
+			})
+			/* meetingDates[
+				{month: '1', day: '15', week: '수요일'},
+				...
+			]
+			*/
+		}
+		setMeetingDates(dateArray)
+	}, [period])
 
 	/** 날짜 클릭 핸들러 (부모컴포넌트에 state값 반환) */
 	const handleDateClick = (date: dateInfo) => {
