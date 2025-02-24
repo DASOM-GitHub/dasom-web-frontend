89748f77a2bb330069b1ceacbb5fad04142277e3
HYEON <titeotty12@gmail.com>
2025-02-24 11:49:51 +0900

    refactor: PythonEditor 분리 및 Main들여쓰기 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -22,11 +22,14 @@ const Main: React.FC = () => {
 
   return (
     <MobileLayout>
+
       <div className='top-44 text-4xl md:text-[24px] lg:text-[36px] font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 via-neutral-400 to-neutral-300 dark:from-neutral-800 dark:via-white dark:to-white'>
         Code Beyond Limits, <br /> Discover <Cover>New Spaces</Cover> with <div className='text-mainColor'>DASOM</div>
       </div>
+      
       <div className='relative top-[600px] max-w-[351px] mx-auto'>
       <PythonEditor /> 
+
         <div className='flex w-full bg-stone-900 min-h-[1px] mb-20' />
         <ActivityStatus year="2024" />
 
@@ -37,6 +40,7 @@ const Main: React.FC = () => {
         <div className='flex items-center justify-center w-full text-[12px] font-pretendardRegular text-white text-center'>
           © 2025 DASOM. ALL RIGHTS RESERVED.
         </div>
+
         {/* 바텀마진 */}
         <div className='flex w-full bg-mainBlack min-h-[20px] mb-50' />
 
@@ -53,6 +57,7 @@ const Main: React.FC = () => {
         {/* 바텀마진 */}
         <div className='flex w-full bg-mainBlack min-h-[60px]' />
       </div>
+      
     </MobileLayout>
   )
 }
