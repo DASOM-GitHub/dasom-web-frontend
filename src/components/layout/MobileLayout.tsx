dafaab7815ba26e8da26a449470524e4d9a841b6
HYEON <74049556+titeotty@users.noreply.github.com>
2025-02-28 23:34:01 +0900

    design: 스크롤바 hidden 설정
    design: 스크롤바 hidden 설정

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -10,7 +10,7 @@ const MobileLayout: React.FC<Props> = ({ children }) => {
 
   return (
     <div
-      className="mx-auto bg-mainBlack"
+      className="mx-auto bg-mainBlack overflow-auto no-scrollbar"
       style={{
         width: width > 480 ? '395px' : '100%', // PC에서는 고정, 모바일에서는 꽉 차게
         height: '100vh',
