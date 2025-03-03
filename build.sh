8a71f8187ebe65ee1f679802147e8cca5f6a54f5
HYEON <74049556+titeotty@users.noreply.github.com>
2025-03-03 20:34:14 +0900

    Revert "version control (#121)"
    This reverts commit 90109b9498d8c1ab1c7ced9ced9844166dacc5d7.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ../
-mkdir output
-cp -R ./dasom-web-frontend/* ./output
-cp -R ./output ./dasom-web-frontend/
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,6 @@ const root = ReactDOM.createRoot(
 )
 root.render(
   <React.StrictMode>
-    <App overflow-auto no-scrollbar />
+    <App />
   </React.StrictMode>
 )
