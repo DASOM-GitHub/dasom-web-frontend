867a7f8efe866624e335a0d0a30a924d3b6ba7ab
limtjdghks <epaktldk0203@gmail.com>
2025-03-03 20:06:14 +0900

    fix: 면접예약 페이지 배너 수정
    * fix: 면접 예약 페이지 InfoBanner 코드 위치 수정

--- src/pages/RecruitMeeting.tsx
+++ src/pages/RecruitMeeting.tsx
@@ -147,11 +147,11 @@ const RecruitMeeting: React.FC = () => {
 		<MobileLayout>
 			<RecruitHeader title='컴퓨터 소프트웨어 공학과 전공 동아리 다솜 34기 모집 폼' />
 			<RecruitUI />
+			<Recruit_InfoBanner
+				message={`1차 서류에 합격되신 점 다시 한번 축하드리며,
+				편하신 날짜의 시간대를 선택해주시길 바랍니다.`}
+			/>
 			<div className='flex flex-col items-center w-full mb-40'>
-				<Recruit_InfoBanner
-					message={`1차 서류에 합격되신 점 다시 한번 축하드리며,
-				 편하신 날짜의 시간대를 선택해주시길 바랍니다.`}
-				/>
 				<div className='mt-8 max-w-[90%]'>
 					<p className='font-pretendardBold text-white mb-4'>면접일</p>
 					<MeetingDateSelector onSelect={handleDateSelect} period={interviewPeriodData} />
