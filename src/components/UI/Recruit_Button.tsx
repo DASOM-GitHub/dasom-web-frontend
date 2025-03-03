62f0c4cfeb25022e7c85a32b93bbc8d3c02e3b31
limtjdghks <epaktldk0203@gmail.com>
2025-03-03 18:28:13 +0900

    fix; recruit 버튼 disabled 상태시 애니메이션 비활성화 수정

--- src/components/UI/Recruit_Button.tsx
+++ src/components/UI/Recruit_Button.tsx
@@ -12,7 +12,7 @@ export const Button: React.FC<ButtonProps> = ({ text, className, onClick, disabl
   return (
     <div className='w-full max-w-[395px] mt-20 mb-20 flex justify-center '>
       <button type="submit"
-        className={`bg-[#00B493] max-w-[395px] h-[30px]  text-white font-pretendardBold px-4 text-[12px] transition-all hover:bg-[#00937A] active:scale-95 ${className}`}
+        className={`bg-[#00B493] max-w-[395px] h-[30px]  text-white font-pretendardBold px-4 text-[12px] transition-all ${!disabled ? 'hover:bg-[#00937A] active:scale-95' : null }  ${className}`}
         onClick={onClick}  
         disabled={disabled}  
       >
