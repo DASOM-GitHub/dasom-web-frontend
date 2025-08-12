7443c24be06f6edeebc58ae98025b375031154f6
Dohyeon <do.hy824@gmail.com>
2025-08-12 17:22:24 +0900

    refactor: import구문 정리

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export * from './admin'
+export * from './news'
+export * from './recruit'
+
+export { default as CoreMembers } from './CoreMembers'
+export { default as FAQ } from './faq/FAQ'
+export { default as Login } from './Login'
+export { default as Main } from './Main'
+export { default as UserMain } from './UserMain'
