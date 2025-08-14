7ef936e0fa691900d882dc39d04d9fcaa8dda242
limtjdghks <epaktldk0203@gmail.com>
2025-08-14 14:56:12 +0900

    fix: 버튼 컴포넌트 분리 (DASOMFE-25)

--- src/components/UI/Recruit_Button.tsx
+++ src/components/UI/Recruit_Button.tsx
@@ -21,13 +21,3 @@ export const Button: React.FC<ButtonProps> = ({
     </div>
   )
 }
-
-export const RecruitInfo_Button:React.FC<ButtonProps> = ({text, onClick}) => {
-  return(
-    <>
-      <button className='font-pretendardRegular bg-subGrey text-mainColor border-mainColor px-5 py-3 rounded-full border-[1px] text-xl transition-all hover:scale-110 active:scale-95' onClick={onClick}>
-        {text}
-      </button>
-    </>
-  )
-}
