e126a8a88f8be3b6968aeacf70113d5379be109d
Youwol <153346797+si-zero@users.noreply.github.com>
2026-02-24 15:06:58 +0900

    merge: 기수 수정 및 모집 폼 수정

--- src/pages/recruit/RecruitSubmit.tsx
+++ src/pages/recruit/RecruitSubmit.tsx
@@ -23,7 +23,7 @@ export const RecruitSubmit: React.FC = () => {
   return (
     <div className='bg-subGrey3' style={{ minHeight: 'calc(100vh - 56px)' }}>
       <MobileLayout>
-        <RecruitHeader title='컴퓨터 소프트웨어 공학과 전공 동아리 다솜 34기 모집 폼' />
+        <RecruitHeader title='컴퓨터 소프트웨어 공학과 전공 동아리 다솜 35기 모집 폼' />
         <RecruitUI />
         <Recruit_InfoBanner
           message={`다솜에 지원해주셔서 감사합니다.
